d1d1293b3d5ce33bf172321fe66f85fe4f1a5e5b
Chocobozzz <me@florianbigard.com>
2020-08-21 14:47:09 +0200

    Improve webpack template

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,19 @@ const path = require("path")
 
 const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin")
 
-let config = {
-  entry: "./client/common-client-plugin.js",
+const clientFiles = [
+  'common-client-plugin.js'
+]
+
+let config = clientFiles.map(f => ({
+  entry: "./client/" + f,
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "./common-client-plugin.js",
+    filename: "./" + f,
     library: "script",
     libraryTarget: "var"
   },
-  plugins: [new EsmWebpackPlugin()]
-}
+  plugins: [ new EsmWebpackPlugin() ]
+}))
 
 module.exports = config
