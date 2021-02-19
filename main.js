f4a565ab67c8469900ea26adeef6578fd7049293
John Livingston <git@john-livingston.fr>
2021-02-19 10:11:42 +0100

    Fix settings privacy.

--- main.js
+++ main.js
@@ -12,28 +12,36 @@ async function register ({
     label: 'Activate chat for all lives',
     type: 'input-checkbox',
     default: false,
-    descriptionHTML: 'If checked, a chat will be added to all lives.'
+    descriptionHTML: 'If checked, a chat will be added to all lives.',
+    private: false
   })
   registerSetting({
     name: 'chat-all-non-lives',
     label: 'Activate chat for all non-lives',
     type: 'input-checkbox',
     default: false,
-    descriptionHTML: 'If checked, a chat will be added to all video that are not lives.'
+    descriptionHTML: 'If checked, a chat will be added to all video that are not lives.',
+    private: false
   })
   registerSetting({
     name: 'chat-videos-list',
     label: 'Activate chat for specific videos',
     type: 'input-textarea',
     default: '',
-    descriptionHTML: 'Videos UUIDs for which we want a chat. Can be non-live videos. One per line. Don\'t add private videos, the UUIDs will be send to frontend.'
+    descriptionHTML: 'Videos UUIDs for which we want a chat. ' +
+      'Can be non-live videos. One per line. ' + 
+      'Don\'t add private videos, the UUIDs will be send to frontend.',
+    private: false
   })
   registerSetting({
     name: 'chat-uri',
     label: 'Webchat url',
     type: 'input',
     default: '',
-    descriptionHTML: 'The webchat url. An iframe will be created pointing to this url. The placeholder {{VIDEO_UUID}} will be replace by the video UUID if present. Example : https://my_domain/conversejs.html?room=video_{{VIDEO_UUID}}.'
+    descriptionHTML: 'The webchat url. An iframe will be created pointing to this url. ' +
+      'The placeholder {{VIDEO_UUID}} will be replace by the video UUID if present. ' +
+      'Example : https://my_domain/conversejs.html?room=video_{{VIDEO_UUID}}.',
+    private: false
   })
   
 }
