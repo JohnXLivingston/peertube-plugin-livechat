d45893a22c9eb6b3c098ce95d46cb24093a32ebf
John Livingston <git@john-livingston.fr>
2021-11-22 14:50:55 +0100

    ConverseJS theme: Fix margin when hidding avatars.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   rules: {
     'selector-class-pattern': [
       // extending the kebab-case to accept ConverseJS class names.
-      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*(__[a-z]+)?$',
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*((__|--)[a-z]+(-[a-z0-9]+)*)?$',
       {
         message: 'Expected class selector to be kebab-case, or ConverseJS-style.',
       }
