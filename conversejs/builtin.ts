74236028ac96fbebd4bae307d0b5bd7f4ffc957f
John Livingston <git@john-livingston.fr>
2021-11-23 18:10:01 +0100

    Fix iframe unwanted horizontal scrollbar.

--- conversejs/builtin.ts
+++ conversejs/builtin.ts
@@ -97,6 +97,8 @@ window.initConverse = async function initConverse ({
     const body = document.querySelector('body')
     if (body) {
       body.classList.add('livechat-iframe')
+      // prevent horizontal scrollbar when in iframe. (don't know why, but does not work if done by CSS)
+      body.style.overflowX = 'hidden'
     }
   }
 
