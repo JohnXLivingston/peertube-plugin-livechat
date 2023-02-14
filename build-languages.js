e6d215358b6b6b1f62184d0ba2a10cfe1da2846d
John Livingston <git@john-livingston.fr>
2023-02-14 15:40:31 +0100

    Preparing settings translations:
    Implementing a new translating system, so we can more easily translate
    settings strings. Indeed, these strings are difficulte to embed in
    JSON format.
    
    This commit add the settings Yml files parsing.
    
    Related to #153.

--- build-languages.js
+++ build-languages.js
@@ -1,76 +1,154 @@
 /**
  * This script is used to build the translations files.
- * 
+ *
  * Indeed, some strings used in plugin settings are not easy to write
  * in JSON format (contains html, double quotes, line breaks, ...).
  * So we use YAML files to translates these strings,
  * including the english version.
- * 
+ *
  * This scripts takes the standard JSON files, the YAML translation files,
  * and mixes everything up in dist/languages/xx.json.
  */
 const fs = require('fs')
 const path = require('path')
+const YAML = require('yaml')
 
-function getAllLangs () {
-  const packagejson = require('./package.json')
-  const translations = packagejson.translations || {}
-  return Object.values(translations).map(filename => {
-    return filename.match(/^.*\/(\w+)\.json$/)[1]
-  })
-}
+class BuildLanguages {
+  destinationDir = null
+  langs = []
+  translationsStrings = {}
+  monoLingualReferences = {}
 
-function initTranslationStrings (langs) {
-  console.log('Initializing translations strings...')
-  const translationsStrings = {}
-  for (l of langs) {
-    translationsStrings[l] = {}
+  constructor () {
+    this.destinationDir = path.resolve(__dirname, 'dist', 'languages')
   }
-  return translationsStrings
-}
 
-async function readJSONTranslations(langs, translationsStrings) {
-  console.log('Reading standard JSON translation strings...')
-  for (l of langs) {
-    const filePath = path.resolve(__dirname, 'languages', l + '.json')
+  async generateFiles () {
+    await this.loadLangs()
+    await this.initTranslationStrings()
+    await this.readJSONTranslations()
+    await this.readSettingsTranslations()
+
+    await this.ensureDestinationDir()
+    await this.writeJSONTranslations()
+    await this.writeMonoLingualReferences()
+  }
+
+  loadLangs () {
+    const packagejson = require('./package.json')
+    const translations = packagejson.translations || {}
+    this.langs = Object.values(translations).map(filename => {
+      return filename.match(/^.*\/(\w+)\.json$/)[1]
+    })
+    console.log('Existings languages: ', this.langs)
+  }
+  
+  initTranslationStrings () {
+    console.log('Initializing translations strings...')
+    const translationsStrings = {}
+    for (const l of this.langs) {
+      translationsStrings[l] = {}
+    }
+    this.translationsStrings = translationsStrings
+  }
+  
+  async readJSONTranslations () {
+    console.log('Reading standard JSON translation strings...')
+    const translationsStrings = this.translationsStrings
+    for (const l of this.langs) {
+      const filePath = path.resolve(__dirname, 'languages', l + '.json')
+      if (!fs.existsSync(filePath)) {
+        console.warn(`File ${filePath} missing, ignoring.`)
+        continue
+      }
+      const t = require(filePath)
+      for (const k in t) {
+        const v = t[k]
+        if (k in translationsStrings[l]) {
+          throw new Error(`Duplicate translation for key ${k} in lang ${l}.`)
+        }
+        translationsStrings[l][k] = v
+      }
+    }
+  }
+
+  async readSettingsTranslations () {
+    console.log('Reading Settings Yaml translation strings...')
+
+    // First we must get the english reference file,
+    // that will give us the keys to use in final JSON.
+
+    const reference = await this.getYmlFileContent(path.resolve(__dirname, 'languages', 'settings.en.yml'))
+    this.monoLingualReferences['settings'] = reference
+
+    const translationsStrings = this.translationsStrings
+    for (const l of this.langs) {
+      const filePath = path.resolve(__dirname, 'languages', 'settings.' + l + '.yml')
+      const o = await this.getYmlFileContent(filePath)
+
+      for (const k in o) {
+        if (!(k in reference)) {
+          throw new Error(`File ${filePath} contains unknown keys. Key=${k}.`)
+        }
+        const newKey = reference[k]
+        this.translationsStrings[l][newKey] = o[k]
+      }
+    }
+  }
+
+  async getYmlFileContent (filePath) {
     if (!fs.existsSync(filePath)) {
       console.warn(`File ${filePath} missing, ignoring.`)
-      continue
+      return {}
     }
-    const t = require(filePath)
-    for (k in t) {
-      const v = t[k]
-      if (k in translationsStrings[l]) {
-        throw new Error(`Duplicate translation for key ${k} in lang ${l}.`)
+
+    const content = await fs.promises.readFile(filePath, 'utf8')
+    const o = YAML.parse(content) || {}
+    for (const k in o) {
+      let v = o[k]
+      if (v === null) {
+        // this value is ok!
+        continue
       }
-      translationsStrings[l][k] = v
+      if ((typeof v) !== 'string') {
+        throw new Error(`File ${filePath} contains strings that are not strings! Key=${k}`)
+      }
+
+      // We are normalizing the string, to avoid problems.
+      // As it is supposed to be html, we will strip newlines and multiple adjacent spaces.
+      v = v.replace(/\n/g, ' ')
+      v = v.replace(/\s\s+/g, ' ')
+      v = v.trim()
+      o[k] = v
+    }
+    return o
+  }
+
+  async ensureDestinationDir () {
+    if (!fs.existsSync(this.destinationDir)) {
+      await fs.promises.mkdir(this.destinationDir)
+    }
+  }
+
+  async writeJSONTranslations () {
+    console.log('Writing JSON files...')
+    for (const l of this.langs) {
+      const filePath = path.resolve(this.destinationDir, l + '.json')
+      await fs.promises.writeFile(filePath, JSON.stringify(this.translationsStrings[l]))
+    }
+  }
+
+  async writeMonoLingualReferences () {
+    console.log('Writing JSON reference files...')
+    for (const name in this.monoLingualReferences) {
+      const filePath = path.resolve(this.destinationDir, name + '.reference.json')
+      await fs.promises.writeFile(filePath, JSON.stringify(this.monoLingualReferences[name]))
     }
   }
 }
 
-async function writeJSONTranslations (langs, translationsStrings) {
-  console.log('Writing JSON files...')
-  const dir = path.resolve(__dirname, 'dist', 'languages')
-  if (!fs.existsSync(dir)) {
-    await fs.promises.mkdir(dir)
-  }
-  for (l of langs) {
-    const filePath = path.resolve(dir, l + '.json')
-    await fs.promises.writeFile(filePath, JSON.stringify(translationsStrings[l]))
-  }
-}
-
-async function generateFiles () {
-  const langs = getAllLangs()
-  console.log('Existings languages: ', langs)
-  
-  const translationsStrings = initTranslationStrings(langs)
-  await readJSONTranslations(langs, translationsStrings)
-
-  await writeJSONTranslations(langs, translationsStrings)
-}
-
-generateFiles().then(() => {}, (err) => {
+const bl = new BuildLanguages()
+bl.generateFiles().then(() => {}, (err) => {
   console.error(err)
   throw err
 })
