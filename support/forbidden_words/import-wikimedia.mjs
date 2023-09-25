16d979c4fd3ec1c5c08ffccd49d6b92e2314b64d
John Livingston <git@john-livingston.fr>
2023-09-25 17:18:42 +0200

    license

--- support/forbidden_words/import-wikimedia.mjs
+++ support/forbidden_words/import-wikimedia.mjs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 John Livingston <https://www.john-livingston.fr/>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import { Command } from 'commander'
 
 const program = new Command()
