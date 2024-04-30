b308d82f16e8d6cae448d5885d396c4eb50e6e87
John Livingston <git@john-livingston.fr>
2024-04-30 19:27:27 +0200

    Task lists WIP

--- conversejs/custom/plugins/tasks/templates/muc-task.js
+++ conversejs/custom/plugins/tasks/templates/muc-task.js
@@ -1,8 +1,17 @@
 import { html } from 'lit'
 
 export default function tplMucTask (task) {
+  const done = task.get('done')
   return html`
     <div class="">
-      Task: ${task.get('name')}
+      <input
+        type="checkbox"
+        class="form-check-input"
+        .checked=${done === true}
+        @click=${(_ev) => {
+          task.save('done', !done)
+        }}
+      />
+      ${task.get('name')}
     </div>`
 }
