c6513f922b12c478829d1708e243306da58a27a8
John Livingston <git@john-livingston.fr>
2024-06-10 17:17:15 +0200

    Custom emojis: increasing the limit.

--- shared/lib/emojis.ts
+++ shared/lib/emojis.ts
@@ -6,4 +6,4 @@ export const maxSize: number = 30 * 1024
 export const allowedExtensions = ['png', 'jpg', 'jpeg', 'gif']
 export const inputFileAccept = ['image/jpg', 'image/png', 'image/gif']
 export const allowedMimeTypes = ['image/jpg', 'image/png', 'image/gif']
-export const maxEmojisPerChannel = 100
+export const maxEmojisPerChannel = 200
