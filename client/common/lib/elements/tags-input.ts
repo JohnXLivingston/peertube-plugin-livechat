b6f196d07c432b5127cb436e055e384d3aea6037
John Livingston <git@john-livingston.fr>
2024-06-13 12:03:11 +0200

    SPDX

--- client/common/lib/elements/tags-input.ts
+++ client/common/lib/elements/tags-input.ts
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2024 Mehdi Benadel <https://mehdibenadel.com>
+// SPDX-FileCopyrightText: 2024 John Livingston <https://www.john-livingston.fr/>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
