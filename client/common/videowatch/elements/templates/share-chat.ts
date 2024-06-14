76f014ed60292043350e6e2c09f69541660d63ca
John Livingston <git@john-livingston.fr>
2024-06-14 15:56:30 +0200

    Missing SPDX

--- client/common/videowatch/elements/templates/share-chat.ts
+++ client/common/videowatch/elements/templates/share-chat.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 John Livingston <https://www.john-livingston.fr/>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import type { ShareChatElement } from '../share-chat'
 import { html, TemplateResult } from 'lit'
 import { ptTr } from '../../../lib/directives/translation'
