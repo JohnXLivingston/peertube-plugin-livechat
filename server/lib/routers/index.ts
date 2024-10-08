77ec284ade0d31b269ece540d4711e981a81db3a
John Livingston <git@john-livingston.fr>
2024-10-08 16:53:55 +0200

    Fix build & CI

--- server/lib/routers/index.ts
+++ server/lib/routers/index.ts
@@ -13,7 +13,12 @@ async function initRouters (options: RegisterServerOptions): Promise<void> {
   const { getRouter } = options
 
   const router = getRouter()
-  router.get('/ping', (req: Request, res: Response, _next: NextFunction) => res.json({ message: 'pong' }))
+  router.get(
+    '/ping',
+    (req: Request, res: Response, _next: NextFunction) => {
+      res.json({ message: 'pong' })
+    }
+  )
 
   router.use('/webchat', await initWebchatRouter(options))
   router.use('/settings', await initSettingsRouter(options))
